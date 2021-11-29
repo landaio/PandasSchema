bbcd33c9253e17a9528d712312e232e207d3a158
Amir <amir@landa.app>
2021-11-29 12:13:47 +0200

    assign column object to warning

--- pandas_schema/validation.py
+++ pandas_schema/validation.py
@@ -229,9 +229,10 @@ class IsDtypeValidation(_BaseValidation):
     def get_errors(self, series: pd.Series, column: 'column.Column' = None):
         if not np.issubdtype(series.dtype, self.dtype):
             return [ValidationWarning(
-                'The column {} has a dtype of {} which is not a subclass of the required type {}'.format(
+                message='The column {} has a dtype of {} which is not a subclass of the required type {}'.format(
                     column.name if column else '', series.dtype, self.dtype
-                )
+                ),
+                column=column
             )]
         else:
             return []
